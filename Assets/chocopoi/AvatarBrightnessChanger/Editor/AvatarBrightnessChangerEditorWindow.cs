416ead25af2cd6aa1482809494698525394f9288
poi-vrc <77053052+poi-vrc@users.noreply.github.com>
2022-04-30 18:37:22 +0800

    Initial commit

---
+++ Assets/chocopoi/AvatarBrightnessChanger/Editor/AvatarBrightnessChangerEditorWindow.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AvatarBrightnessChangerEditorWindow : EditorWindow
+{
+    /// <summary>
+    /// Initialize the Avatar Brightness Changer window
+    /// </summary>
+    [MenuItem("Tools/chocopoi/AvatarBrightnessChanger", false, 0)]
+    public static void Init()
+    {
+        AvatarBrightnessChangerEditorWindow window = (AvatarBrightnessChangerEditorWindow)GetWindow(typeof(AvatarBrightnessChangerEditorWindow));
+        window.titleContent = new GUIContent("AvatarBrightnessChanger");
+        window.Show();
+    }
+
+    private VRC.SDK3.Avatars.Components.VRCAvatarDescriptor avatar;
+
+    //Referenced and modified from https://answers.unity.com/questions/8500/how-can-i-get-the-full-path-to-a-gameobject.html
+    private static string GetGameObjectPath(Transform transform, string prefix = "", string suffix = "")
+    {
+        string path = transform.name;
+        while (true)
+        {
+            transform = transform.parent;
+
+            if (transform.parent == null)
+            {
+                break;
+            }
+
+            path = transform.name + "/" + path;
+        }
+        return prefix + path + suffix;
+    }
+
+    private void Generate()
+    {
+        if (avatar == null)
+        {
+            return;
+        }
+
+        AnimationClip clip = new AnimationClip();
+
+        SkinnedMeshRenderer[] smrs = avatar.GetComponentsInChildren<SkinnedMeshRenderer>(true);
+        foreach (SkinnedMeshRenderer smr in smrs)
+        {
+            Material[] ms = smr.sharedMaterials;
+            for (int i = 0; i < ms.Length; i++)
+            {
+                if (ms[i].shader.name.Contains("lilToon"))
+                {
+                    Debug.Log("Adding " + ms[i].shader.name);
+                    clip.SetCurve(GetGameObjectPath(smr.transform), typeof(SkinnedMeshRenderer), "material._LightMinLimit", AnimationCurve.Linear(0.0f, 0.0f, 1.0f, 1.0f));
+                }
+            }
+        }
+
+        AssetDatabase.CreateAsset(clip, "Assets/chocopoi/LiltoonBrightnessChanger/test.anim");
+        AssetDatabase.SaveAssets();
+
+        MeshRenderer[] mrs = avatar.GetComponentsInChildren<MeshRenderer>(true);
+        foreach (MeshRenderer mr in mrs)
+        {
+        }
+    }
+
+    public void OnGUI()
+    {
+        avatar = (VRC.SDK3.Avatars.Components.VRCAvatarDescriptor) EditorGUILayout.ObjectField("Avatar", avatar, typeof(VRC.SDK3.Avatars.Components.VRCAvatarDescriptor), true);
+
+        if (GUILayout.Button("Generate Animation"))
+        {
+            Generate();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# AvatarBrightnessChanger
+
+A simple animation + expression menu generator to adjust avatar brightness of various shaders including Liltoon, Poiyomi
